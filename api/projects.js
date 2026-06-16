4a2d8490c1400c84d04a38f3800e43ff0483a454
AlamoJoeW <Joe@alamoairborne.com>
2026-06-15 19:18:15 -0500

    Update projects.js

--- api/projects.js
+++ api/projects.js
@@ -5,9 +5,9 @@ const PROJECT_NAME     = 'fldxRxPZss5l3BFFx'
 const PROJECT_ACTIVE   = 'fldbGd0Ph45S89qoa'
 
 function deriveType(name) {
-  if (name.startsWith('vHive Verizon'))                   return 'verizon'
-  if (/Solar|Wind/.test(name))                            return 'solar_wind'
-  if (/Methane|BP/.test(name))                            return 'methane'
+  if (name.startsWith('vHive Verizon'))  return 'verizon'
+  if (/Solar|Wind/.test(name))           return 'solar_wind'
+  if (/Methane|BP/.test(name))           return 'methane'
   return 'other'
 }
 
@@ -15,8 +15,8 @@ export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' })
 
   try {
-    const filter = `${${PROH¡CT_ACTIVE}}='Yes'`
-    const records = await airtableGetAll(PROJECTS_TABLE, filter, [PROJECT_NAME, PP¯JECT_ACTIVE])
+    const filter = `{${PROJECT_ACTIVE}}='Yes'`
+    const records = await airtableGetAll(PROJECTS_TABLE, filter, [PROJECT_NAME, PROJECT_ACTIVE])
     const projects = records
       .map(r => {
         const name = r.fields[PROJECT_NAME] || ''
