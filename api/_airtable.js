05645612faa5d5636f1224e95ada30306ab9a6c8
AlamoJoeW <Joe@alamoairborne.com>
2026-06-16 19:07:39 -0500

    Update _airtable.js

--- api/_airtable.js
+++ api/_airtable.js
@@ -4,125 +4,69 @@ export const BASE_ID = process.env.AIRTABLE_BASE || 'app3uLCFgt3Y0aPaa'
 export const API_KEY = process.env.AIRTABLE_API_KEY
 
 export const TABLES = {
-  PILOTS:             'tblYVHjbcI46iQ4EB',
-  COLLECTION_ASSETS:  'tbl1y4oOzEAhf0a4S',
-  EOD_REPORTS:        'tblxlNUqgFy251qha',
-  PREFLIGHT:          'tbl3XS1n9edeDuLOn',
-  PROJECTS:           'tblHYYTZxN8IHRNwA',
-  AIRCRAFT_SENSOR:    'tblbbnIwzQJRa4mpo',
-  DAILY_ASSIGNMENTS:  'tblaNln93bTaldquV',
+  PILOTS: 'tblYVHjbcI46iQ4EB',
+  COLLECTION_ASSETS: 'tbl1y4oOzEAhf0a4S',
+  EOD_REPORTS: 'tblxlNUqgFy251qha',
 }
 
 export const FIELDS = {
   // Pilots
-  PILOT_EMAIL:          'fldtJh69QILzlTgMY',
-  PILOT_PASSWORD:       'fld8mIJRKQnPqAz2p',
-  PILOT_FIRST_NAME:     'fldj99i8QHMNP1N9a',
-  PILOT_DISPLAY_NAME:   'fldKEKxHbI9lRCXTR',
+  PILOT_EMAIL: 'fldtJh69QILzlTgMY',
+  PILOT_PASSWORD: 'fld8mIJRKQnPqAz2p',
+  PILOT_FIRST_NAME: 'fldj99i8QHMNP1N9a',
+  PILOT_DISPLAY_NAME: 'fldKEKxHbI9lRCXTR', // formula: full name
 
   // Collection Assets
-  SITE_ID:              'fldeMYc6CwJOqKKNh',
-  FUZE_ID:              'fld6vEZSPDY7KW38S',
-  COLLECTION_STATUS:    'fld1EPWNQo3zBPsK7',
-  SITE_ISSUE:           'fldovKW04qQhgVnu7',
-  PILOT_ASSIGNED:       'fldxdjMwaTmXabapY',
-  SUB_PROJECT:          'fldbhgVAo5Zw5kTAD',
-  ADDRESS:              'fldcDkKV2vyn25UC5',
-  CITY:                 'fldlxY0GC60x2Sdhc',
-  STATE:                'fldSmMUwJ3XV9P4wt',
-  ZIP:                  'fldwQOL2kGzhS7KNm',
-  SITE_STRUCTURE_TYPE:  'fldEarowciBZ1xo2B',
-  STRUCTURE_HEIGHT:     'fldIDfYAc79cbgdf5',
-  AIRPORT:              'fldsh2jGThrn8lPn3',
-  AIRSPACE:             'fldeE5RD1XLMIOoXo',
-  LATITUDE:             'fldfrOZfgkRgF3fVY',
-  LONGITUDE:            'fldBRvkT3ZwBIJS05',
-  DATE_ADDED:           'fldvZjxOCRfcT055V',
-  MAP_COLOR:            'fldd8KeiQAeFXc2cR',
+  SITE_ID: 'fldeMYc6CwJOqKKNh',
+  FUZE_ID: 'fld6vEZSPDY7KW38S',
+  COLLECTION_STATUS: 'fld1EPWNQo3zBPsK7',
+  SITE_ISSUE: 'fldovKW04qQhgVnu7',
+  PILOT_ASSIGNED: 'fldxdjMwaTmXabapY',
+  SUB_PROJECT: 'fldbhgVAo5Zw5kTAD',
+  ADDRESS: 'fldcDkKV2vyn25UC5',
+  CITY: 'fldlxY0GC60x2Sdhc',
+  STATE: 'fldSmMUwJ3XV9P4wt',
+  ZIP: 'fldwQOL2kGzhS7KNm',
+  SITE_STRUCTURE_TYPE: 'fldEarowciBZ1xo2B',
+  STRUCTURE_HEIGHT: 'fldIDfYAc79cbgdf5',
+  AIRPORT: 'fldsh2jGThrn8lPn3',
+  AIRSPACE: 'fldeE5RD1XLMIOoXo',
+  LATITUDE: 'fldfrOZfgkRgF3fVY',
+  LONGITUDE: 'fldBRvkT3ZwBIJS05',
+  DATE_ADDED: 'fldvZjxOCRfcT055V',
+  MAP_COLOR: 'fldd8KeiQAeFXc2cR',
   SITE_STRUCTURE_OWNER: 'fldOTBMryx9tr8hSf',
-  MOB_FEE:              'fldZDb14q18VOR2De',
-  PARTIAL_COLLECTION:   'fldcD4EwDU5HkDFua',
-  COLLECTED_APP:        'fldNK7WyoeYeDhgE6',
+  MOB_FEE: 'fldZDb14q18VOR2De',
+  PARTIAL_COLLECTION: 'fldcD4EwDU5HkDFua',
+  COLLECTED_APP: 'fldNK7WyoeYeDhgE6',
 
   // EOD Reports
-  EOD_DATE:             'fldHhWbzHpjzQIQ3n',
-  EOD_PILOT:            'fldnARlXlU1Y1lov4',
-  EOD_FULL_COLLECTION:  'fldNvJt3DszkVZHyC',
+  EOD_DATE: 'fldHhWbzHpjzQIQ3n',
+  EOD_PILOT: 'fldnARlXlU1Y1lov4',
+  EOD_FULL_COLLECTION: 'fldNvJt3DszkVZHyC',
   EOD_PARTIAL_COLLECTION: 'fldx1NbBv3bFvBoJf',
-  EOD_MOBILIZATION:     'fldEv1OCPrpOhjMqs',
-  EOD_END_LAT:          'fldsXSAqo2HCvDqzg',
-  EOD_END_LNG:          'fldAl7s8VeN3UWQtE',
-
-  // Pre-flight Risk Assessment
-  PREFLIGHT_DATE:       'fld3e4DOx5yYCgbYe',
-  PREFLIGHT_PILOT:      'fldiapaRvwWUjBC4x',
-  PREFLIGHT_PROJECT:    'fldH8N230tZWXxxZr',
-  PREFLIGHT_TRAVEL_DAY: 'fldkUrdlnzkuw8ML5',
-  PREFLIGHT_TRAVELING_TO: 'fldQEK9X7oGDodpGv',
-  PREFLIGHT_VISUAL_OBS: 'fldq9cdjr2Xka4gNU',
-  PREFLIGHT_AIRCRAFT:   'fldXJPP7MDKEaLSCV',
-  PREFLIGHT_ACRES:      'flde2vhvTKKRjxAf2',
-  PREFLIGHT_BASIN:      'fldavOCL4taDWbKjr',
-  PREFLIGHT_FLIGHT_ADDR: 'fldWaWcaL4NOJcdnP',
-  PREFLIGHT_HOSPITAL:   'fldW4Ofn8HB5QDwon',
-  PREFLIGHT_IMSAFE:     'fldkfoTxuuvdDcfBB',
-  PREFLIGHT_WEATHER_CHK: 'fldtG4CncB7BPB0Kc',
-  PREFLIGHT_WEATHER_FCST: 'flda4qHgaJR1Zu7A3',
-  PREFLIGHT_AIRWORTHY:  'fld40658xFfOJ8CFn',
-  PREFLIGHT_AIRWORTHY_N: 'fldM9piS7h91ZgBtP',
-  PREFLIGHT_AIRSPACE:   'fldjlqeEuvawTW4Y3',
-  PREFLIGHT_TFR:        'fldVH0r0hjgPIJneE',
-  PREFLIGHT_LAANC_REQ:  'fldlMlZKaYnNcUR1J',
-  PREFLIGHT_CREW_REST:  'fldn4FW0OroiL214h',
-  PREFLIGHT_CREW_DAYS:  'fld6zYpJ9uxACJxvq',
-  PREFLIGHT_CREW_WORK:  'fld7KAF09V0j2lbb7',
-  PREFLIGHT_WX_WIND:    'fldlMLvrVa6QbAWoP',
-  PREFLIGHT_WX_VIS:     'fldI8QV1OIpbzAukx',
-  PREFLIGHT_WX_CEIL:    'fldmFj1nQ0e7DXVUT',
-  PREFLIGHT_WX_RAIN:    'fldjlZXJQpCCKub7n',
-  PREFLIGHT_WX_TEMP:    'fldT2VZ7fECRALv19',
-  PREFLIGHT_WX_TSTORM:  'fldDZPynJrcqmsoMB',
-  PREFLIGHT_RISK_TOTAL: 'fldlIeYxPDF6fkOqv',
-  PREFLIGHT_ADD_RISKS:  'fld69iUTWmrmVfLxw',
-  PREFLIGHT_MITIGATING: 'fldHYjSRFoMTVH24A',
-  PREFLIGHT_MISSION_OVW: 'fld2u2SXsljOi1L8l',
-  PREFLIGHT_GO_NOGO:    'fldlVErGgP1IJAoLp',
-  PREFLIGHT_NOTES:      'fldY4gVkWEfEhZgFV',
-  PREFLIGHT_EMERG_CONTACT: 'fldpvClkZS1S30rZ7',
-  PREFLIGHT_START_LAT:  'fldAUf6IwteFufphx',
-  PREFLIGHT_START_LNG:  'fldF7blLSgdAq9mNz',
-  PREFLIGHT_EOD_LINK:   'fldMTHfIuLaJoKdtv',
-
-  // Projects
-  PROJECT_NAME:         'fldxRxPZss5l3BFFx',
-  PROJECT_ACTIVE:       'fldbGd0Ph45S89qoa',
-
-  // Aircraft/Sensor
-  AIRCRAFT_NAME:        'flds8icEjiTRwwWno',
-
-  // Daily Assignments
-  DA_PILOT:             'fld5VShFudYtTQ9pm',
-  DA_DATE:              'fldP9zDqFfLSt1qqw',
-  DA_ROUTE:             'fldeg8UtYSPRn4IaN',
-  DA_STATUS:            'fldgp2sUUDa9SoMbz',
+  EOD_MOBILIZATION: 'fldEv1OCPrpOhjMqs',
 }
 
 // All site fields to fetch for the app
-export const SITE_FIELDS = [
-  'fldeMYc6CwJOqKKNh', 'fld6vEZSPDY7KW38S', 'fld1EPWNQo3zBPsK7',
-  'fldovKW04qQhgVnu7', 'fldxdjMwaTmXabapY', 'fldbhgVAo5Zw5kTAD',
-  'fldcDkKV2vyn25UC5', 'fldlxY0GC60x2Sdhc', 'fldSmMUwJ3XV9P4wt',
-  'fldwQOL2kGzhS7KNm', 'fldEarowciBZ1xo2B', 'fldIDfYAc79cbgdf5',
-  'fldsh2jGThrn8lPn3', 'fldeE5RD1XLMIOoXo', 'fldfrOZfgkRgF3fVY',
-  'fldBRvkT3ZwBIJS05', 'fldvZjxOCRfcT055V', 'fldd8KeiQAeFXc2cR',
-  'fldOTBMryx9tr8hSf', 'fldZDb14q18VOR2De', 'fldcD4EwDU5HkDFua',
-  'fldNK7WyoeYeDhgE6',
-]
+export const SITE_FIELDS = Object.values(FIELDS).filter(id =>
+  [
+    'fldeMYc6CwJOqKKNh', 'fld6vEZSPDY7KW38S', 'fld1EPWNQo3zBPsK7',
+    'fldovKW04qQhgVnu7', 'fldxdjMwaTmXabapY', 'fldbhgVAo5Zw5kTAD',
+    'fldcDkKV2vyn25UC5', 'fldlxY0GC60x2Sdhc', 'fldSmMUwJ3XV9P4wt',
+    'fldwQOL2kGzhS7KNm', 'fldEarowciBZ1xo2B', 'fldIDfYAc79cbgdf5',
+    'fldsh2jGThrn8lPn3', 'fldeE5RD1XLMIOoXo', 'fldfrOZfgkRgF3fVY',
+    'fldBRvkT3ZwBIJS05', 'fldvZjxOCRfcT055V', 'fldd8KeiQAeFXc2cR',
+    'fldOTBMryx9tr8hSf', 'fldZDb14q18VOR2De', 'fldcD4EwDU5HkDFua',
+    'fldNK7WyoeYeDhgE6',
+  ].includes(id)
+)
 
 export async function airtableGet(table, params = {}) {
   const qs = new URLSearchParams()
   qs.set('returnFieldsByFieldId', 'true')
   if (params.filterByFormula) qs.set('filterByFormula', params.filterByFormula)
+  if (params.view) qs.set('view', params.view)
   if (params.fields) params.fields.forEach(f => qs.append('fields[]', f))
   if (params.offset) qs.set('offset', params.offset)
   if (params.maxRecords) qs.set('maxRecords', params.maxRecords)
@@ -141,7 +85,10 @@ export async function airtablePatch(table, recordId, fields) {
   const url = `https://api.airtable.com/v0/${BASE_ID}/${table}/${recordId}`
   const res = await fetch(url, {
     method: 'PATCH',
-    headers: { Authorization: `Bearer ${API_KEY}`, 'Content-Type': 'application/json' },
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify({ fields }),
   })
   if (!res.ok) {
@@ -155,7 +102,10 @@ export async function airtablePost(table, fields) {
   const url = `https://api.airtable.com/v0/${BASE_ID}/${table}`
   const res = await fetch(url, {
     method: 'POST',
-    headers: { Authorization: `Bearer ${API_KEY}`, 'Content-Type': 'application/json' },
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify({ fields }),
   })
   if (!res.ok) {
@@ -165,15 +115,17 @@ export async function airtablePost(table, fields) {
   return res.json()
 }
 
-export async function airtableGetAll(table, filterByFormula, fields) {
+// Paginate through all records matching a filter (optionally scoped to a view)
+export async function airtableGetAll(table, filterByFormula, fields, view = null) {
   const records = []
   let offset = null
   do {
     const params = { filterByFormula, fields, pageSize: 100 }
     if (offset) params.offset = offset
+    if (view) params.view = view
     const data = await airtableGet(table, params)
     records.push(...data.records)
     offset = data.offset || null
   } while (offset)
   return records
-}
+    }
